ad50906a8997a56d02f7e745c0d8e78513e181d8
lxp_funny <lxp_funny@163.com>
2015-03-25 20:39:20 +0800

    git跟踪

--- maven-parent/maven-web/src/main/java/controller/SpringMvcController.java
+++ maven-parent/maven-web/src/main/java/controller/SpringMvcController.java
@@ -1,15 +1,26 @@
 package controller;
 
+import java.util.List;
+
+import model.TUser;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import service.IUserService;
+
+
 
 @Controller
 @RequestMapping("/user/*")
 public class SpringMvcController {
+	
+	@Autowired
+	private IUserService userServiceImpl;
 	
 	//重定向视图
 	
@@ -21,6 +32,8 @@ public class SpringMvcController {
 	
 	@RequestMapping("login1.do")
 	public String login1(Model model){
+		
+		List<TUser> list = userServiceImpl.getUser();
 		
 		return "loginSuccess";
 	}
